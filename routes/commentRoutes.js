1815143987dfe276d9f6863d5ef6e9f49705540e
Aishat Akinyemi <akinyemiomolola99@gmail.com>
2024-05-07 14:29:29 +0100

    Upload and delete comment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const {
-  createComment
+  createComment,
+  updateComment,
+  deleteComment
 } = require("../controllers/commentControllers");
 const { authGuard } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", authGuard,  createComment);
+router.route("/:commentId").put(authGuard,  updateComment).delete(authGuard,  deleteComment);
 
 
 module.exports = router;
