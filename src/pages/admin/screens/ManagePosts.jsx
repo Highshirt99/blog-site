e7906b9324d80caa94f88992989ce3fa75fb379c
Aishat Akinyemi <akinyemiomolola99@gmail.com>
2024-05-12 16:11:35 +0100

    Paginate data

--- src/pages/admin/screens/ManagePosts.jsx
+++ src/pages/admin/screens/ManagePosts.jsx
@@ -1,44 +1,54 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import { getAllPosts } from "../../../services/index/posts";
 import { useQuery } from "@tanstack/react-query";
 import { images, stables } from "../../../constants";
+import Pagination from "../../../components/Pagination";
 
 const ManagePosts = () => {
+
   const [searchKeyword, setSearchKeyword] = useState("");
-  const [currentPage, setCurrentPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1);
   const {
-    data: postData,
+    data: postsData,
     isLoading,
     isFetching,
-    refetch
+    refetch,
   } = useQuery({
     queryFn: () => getAllPosts(searchKeyword, currentPage),
     queryKey: ["posts"],
   });
 
+  useEffect(() => {
+    refetch();
+  }, [refetch, currentPage]);
+
   const searchKeywordHandler = (e) => {
-    const {value} = e.target
+    const { value } = e.target;
     setSearchKeyword(value);
   };
 
   const submitSearchKeywordHandler = (e) => {
     e.preventDefault();
-    refetch()
-  }
+    setCurrentPage(1);
+    refetch();
+  };
   return (
     <div>
       <h1 className="text-2xl font-semibold">Manage Posts</h1>
-      <div className=" px-4 mx-auto w-full">
+      <div className="w-full px-4 mx-auto ">
         <div className="py-8">
           <div className="flex flex-row justify-between w-full mb-1 sm:mb-0">
             <h2 className="text-2xl leading-tight">Users</h2>
             <div className="text-end">
-              <form className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0" onSubmit={submitSearchKeywordHandler}>
-                <div className=" relative ">
+              <form
+                className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0"
+                onSubmit={submitSearchKeywordHandler}
+              >
+                <div className="relative ">
                   <input
                     type="text"
                     id='"form-subscribe-Filter'
-                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="post title"
                     onChange={searchKeywordHandler}
                     value={searchKeyword}
@@ -91,12 +101,12 @@ const ManagePosts = () => {
                 <tbody>
                   {isLoading || isFetching ? (
                     <tr>
-                      <td colSpan="5" className="py-10 text-center w-full">
+                      <td colSpan="5" className="w-full py-10 text-center">
                         Loading...
                       </td>
                     </tr>
                   ) : (
-                    postData?.data.map((post) => (
+                    postsData?.data.map((post) => (
                       <tr key={post._id}>
                         <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
                           <div className="flex items-center">
@@ -110,7 +120,7 @@ const ManagePosts = () => {
                                         post.photo
                                       : images.samplePostImage
                                   }
-                                  className="mx-auto object-cover rounded-lg w-10 aspect-square"
+                                  className="object-cover w-10 mx-auto rounded-lg aspect-square"
                                 />
                               </a>
                             </div>
@@ -145,7 +155,8 @@ const ManagePosts = () => {
                             {post.tags.length > 0 ? (
                               post.tags.map((tag, index) => (
                                 <p key={tag}>
-                                  {tag}{post.tags.length - 1 !== index && ","}
+                                  {tag}
+                                  {post.tags.length - 1 !== index && ","}
                                 </p>
                               ))
                             ) : (
@@ -166,64 +177,16 @@ const ManagePosts = () => {
                   )}
                 </tbody>
               </table>
-              <div className="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
-                <div className="flex items-center">
-                  <button
-                    type="button"
-                    className="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100"
-                  >
-                    <svg
-                      width="9"
-                      fill="currentColor"
-                      height="8"
-                      className=""
-                      viewBox="0 0 1792 1792"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"></path>
-                    </svg>
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-indigo-500 bg-white border-t border-b hover:bg-gray-100 "
-                  >
-                    1
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                  >
-                    2
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-gray-600 bg-white border-t border-b hover:bg-gray-100"
-                  >
-                    3
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                  >
-                    4
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100"
-                  >
-                    <svg
-                      width="9"
-                      fill="currentColor"
-                      height="8"
-                      className=""
-                      viewBox="0 0 1792 1792"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
+
+              {!isLoading && (
+                <Pagination
+                  onPageChange={(page) => setCurrentPage(page)}
+                  currentPage={currentPage}
+                  totalPageCount={JSON.parse(
+                    postsData?.headers?.["x-totalpagecount"]
+                  )}
+                />
+              )}
             </div>
           </div>
         </div>
