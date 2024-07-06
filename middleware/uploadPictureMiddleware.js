03db1310d5fedf8a4af4be37d08d508d3dc3616e
Aishat Akinyemi <akinyemiomolola99@gmail.com>
2024-07-06 10:12:09 +0100

    Change image upload limit

--- middleware/uploadPictureMiddleware.js
+++ middleware/uploadPictureMiddleware.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
 const uploadPicture = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1000000, // 1MB
+    fileSize: 15 * 1000000, // 15MB
   },
   fileFilter: function (req, file, cb) {
     let ext = path.extname(file.originalname).toLowerCase();
